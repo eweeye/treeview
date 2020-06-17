efb575c1d8f707f4f05980b755b1b581c6e10b95
Scott Hodgson <5344448@gmail.com>
2020-06-16 21:34:38 -0400

    Added button styling

--- src/js/treeview.js
+++ src/js/treeview.js
@@ -1,14 +1,47 @@
 eweeye.TreeView = (function() {
     {   
         var _constLibrary = "eweeye";
-        var _constTree = "tr";
-        var _constId = "id";
-        var _constChildren = "ch";
+        var _constTreeView = "treeview";
+        var _constNode = "node";
         var ui = {
             Trees: {},
             Nodes: {},
             Types: {
+                "Folder" : {
+                    "Options" : {
+                        IsExpandable: true
+                    },
+                    "Content" : function(item) {
+                        var span = document.createElement('span');
+                        var text = "";
+                        if (item === null) 
+                            text = "";
+                        switch (typeof item) {
+                            case "string":
+                                text = item;
+                                break;
+                            default:
+                                text = "";
+                                break;
+                        } 
+                        span.appendChild(document.createTextNode(text));
+                        return span;
+                    },
+                    "Icon" : function(node) {
+                        var span = document.createElement('span');
+                        span.classList.add('fas');
+                        if (node.Expanded) {
+                            span.classList.add('fa-folder-open');
+                        } else {
+                            span.classList.add('fa-folder');
+                        }
+                        return span;
+                    }
+                },
                 "Primitive" : {
+                    "Options" : {
+                        IsExpandable: false
+                    },
                     "Content" : function(item) {
                         var span = document.createElement('span');
                         var text = "";
@@ -76,6 +109,19 @@ eweeye.TreeView = (function() {
 
             },
 
+            GetType: function(name, treeId) {
+                var type = ui.Types.Primitive;
+                if (name) {
+                    if (ui.Types.hasOwnProperty(name)) {
+                        type = ui.Types[name];
+                    }
+                    if (treeId && ui.Trees[treeId].Types.hasOwnProperty(name)) {
+                        type = ui.Trees[treeId].Types[node.Type];
+                    }
+                }
+                return type;
+            },
+
             Add: function(treeId, parentId, id, value, type) {
                 if (!ui.Trees.hasOwnProperty(treeId)) {
                     ui.Trees[treeId] = {
@@ -95,16 +141,22 @@ eweeye.TreeView = (function() {
                     console.error("Id already exists in DOM");
                     return;
                 }
+                var nodeType = ui.GetType(type, treeId);
                 var render = true;
                 if (parentId) {
                     var tempParentId = parentId;
-                    var count = 0;
+                    var tempParentType;
                     while (tempParentId) {
-                        console.log(count);
                         if (!ui.Nodes.hasOwnProperty(tempParentId)) {
-                            render = false;
+                            console.error("Parent does not exist");
+                            return;
                         } else {
-                            var parent = ui.Nodes[tempParentId];                             
+                            var parent = ui.Nodes[tempParentId];
+                            tempParentType = ui.GetType(parent.Type, treeId);
+                            if (!tempParentType.Options.IsExpandable) {
+                                console.error("Parent does not support children");
+                                return;
+                            }
                             if (parent.Rendered === false || parent.Visible === false || parent.Expanded === false) {
                                 render = false;
                                 break;
@@ -118,37 +170,49 @@ eweeye.TreeView = (function() {
                     Id: id,
                     Tree: treeId,
                     Parent: parentId,
-                    Children: {},
                     Rendered: false,
-                    Expanded: false,
                     Visible: false,
                     Type: type,
                     Value: value
                 }; 
+                if (nodeType.Options.IsExpandable) {
+                    node.Children = {};
+                    node.Expanded = false;
+                }
                 ui.Nodes[id] = node;
                 ui.Trees[treeId].Nodes[id] = id;
                 if (parentId) {
-                    ui.Nodes[parentId].Children[id] = id;
+                    var parentType = ui.GetType(ui.Nodes[parentId].Type, treeId);
+                    if (parentType.Options.IsExpandable) {
+                        ui.Nodes[parentId].Children[id] = id;
+                    }
                 }
                 if (render) {
                     ui.Render(node);
                 }
             },
 
+            // Function to render a node into the DOM
             Render: function(node) {
+                // Determine the UL that the node should be added under
+                // Default: Root of the tree
                 var parentId = node.Tree;
+                // Alternate: Parent within the tree
                 if (node.Parent) {
                     parentId = node.Parent;
                 }
+                // Verify the containing UL exists in the DOM already
                 var parentUL;
                 var selectedElement = document.getElementById(parentId);
                 if (!selectedElement) {
                     console.error("Parent not found in DOM");
                     return;
                 }
+                // If the selected element is a UL, it is where the node is to be added
                 if (selectedElement instanceof HTMLUListElement) {
                     parentUL = selectedElement;
                 } else {
+                    // If the selected element is a LI, it is a list item that may have a UL for children
                     if (selectedElement instanceof HTMLLIElement) {
                         selectedElement = Array.prototype.filter.call(selectedElement.children,
                             function (element) {
@@ -163,19 +227,12 @@ eweeye.TreeView = (function() {
                     console.error("Parent's children not found in DOM");
                     return;
                 }
-                var type = ui.Types.Primitive;
-                if (node.Type) {
-                    if (ui.Types.hasOwnProperty(node.Type)) {
-                        type = ui.Types[node.Type];
-                    }
-                    if (ui.Trees[treeId].Types.hasOwnProperty(node.Type)) {
-                        type = ui.Trees[treeId].Types[node.Type];
-                    }
-                }
+                var type = ui.GetType(node.Type, node.TreeId);
                 var nodeLI = document.createElement('li');
                 nodeLI.id = node.Id;
                 var contentDIV = document.createElement('div');
                 var contentButton = document.createElement('button');
+                contentButton.classList.add(_constNode);
                 contentDIV.appendChild(contentButton);
                 var icon = type? type.Icon? type.Icon(node.Value) : null : null;
                 var content = type? type.Content? type.Content(node.Value) : null : null;
@@ -188,8 +245,10 @@ eweeye.TreeView = (function() {
                 var optionDIV = document.createElement('div');
                 nodeLI.appendChild(contentDIV);
                 nodeLI.appendChild(optionDIV);
-                var nodeUL = document.createElement('ul');
-                nodeLI.appendChild(nodeUL);
+                if (type.Options.IsExpandable) {
+                    var nodeUL = document.createElement('ul');
+                    nodeLI.appendChild(nodeUL);
+                }
                 parentUL.appendChild(nodeLI);   
                 node.Rendered = true;
                 node.Visible = true;  
