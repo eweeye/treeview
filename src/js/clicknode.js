400ca38641f4383170d4cdaeaf3b411269332d7a
Scott Hodgson <5344448@gmail.com>
2020-06-18 09:59:28 -0400

    Click event is able to isolate clicked node id

--- src/js/clicknode.js
+++ src/js/clicknode.js
@@ -1,6 +1,17 @@
 document.addEventListener('click', function(event) {
-    // Only react to treeview node click
-    if (!event.target.matches('.node')) return;
-    // The id of the node is on the LI tag, two levels up: LI > DIV > BUTTON
-    var nodeID = (event.target).parentElement.parentElement;
+    var GetClosest = function (elem, selector) {
+        for ( ; elem && elem !== document; elem = elem.parentNode ) {
+            if ( elem.matches( selector ) ) return elem;
+        }
+        return null;
+    };
+    // look for the containing button
+    var button = GetClosest(event.target, 'button');
+    if (button) {
+        // check for main button of node
+        if (button.matches('.node')) {
+            var li = GetClosest(button, 'li');
+            var nodeId = li.id;
+        }
+    }
 });
